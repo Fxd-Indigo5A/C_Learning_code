797cf6c739d23a9b83c8b7ac04a6832284923dc1
Fxd-Indigo5A <155859457+Fxd-Indigo5A@users.noreply.github.com>
2024-01-10 06:41:44 -0500

    Update test.c

--- 2024-01-09/2024-01-09/test.c
+++ 2024-01-09/2024-01-09/test.c
@@ -347,3 +347,46 @@
 //	return 0;
 //}
 
+
+//10.  whether it is sorted
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int arr[50] = { 0 };
+//	int i = 0;
+//	int flag1 = 0; 
+//	int flag2 = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//		if (i > 0)
+//		{
+//			
+//			if (arr[i] > arr[i - 1])
+//			{
+//				flag1 = 1;
+//			}
+//			else if (arr[i] < arr[i - 1])
+//			{
+//				flag2 = 1;
+//			}
+//			else
+//			{
+//				;
+//			}
+//		}
+//	}
+//
+//	if (flag1 + flag2 <= 1)
+//	{
+//		printf("sorted!\n");
+//	}
+//	else
+//	{
+//		printf("unsorted!\n");
+//	}
+//	return 0;
+//}
+
